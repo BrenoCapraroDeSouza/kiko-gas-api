f46497690d72d289a3d3b1e8241be473908e0045
RafaelMotaAlvess <leafardzn@gmail.com>
2024-09-21 20:55:24 -0300

    feat: add password hashing and check for existing user by email

--- src/service/resale/register-service.ts
+++ src/service/resale/register-service.ts
@@ -1,6 +1,7 @@
 import { Resale } from "@prisma/client";
 import { ResaleRepository } from "../../respositories/resales-repository";
 import { UsersRepository } from "../../respositories/users-repository";
+import { hash } from "bcryptjs";
 
 interface RegisterServiceRequest {
   name: string;
@@ -31,9 +32,17 @@ export class RegisterService {
   async execute(
     data: RegisterServiceRequest
   ): Promise<RegisterServiceResponse> {
+    const password_hash = await hash(data.password, 6);
+
+    const userWithSameEmail = await this.userRepository.findByEmail(data.email);
+
+    if (userWithSameEmail) {
+      throw new Error("User already exists");
+    }
+
     const user = await this.userRepository.create({
       email: data.email,
-      password: data.password,
+      password: password_hash,
       userType: "RESALE",
     });
 
