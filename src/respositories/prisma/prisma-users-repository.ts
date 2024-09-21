b7fa7702fdcd5dbae0a100b5adeedbebdcdd25dd
RafaelMotaAlvess <leafardzn@gmail.com>
2024-09-21 20:55:07 -0300

    feat: add method to find user by email

--- src/respositories/prisma/prisma-users-repository.ts
+++ src/respositories/prisma/prisma-users-repository.ts
@@ -1,11 +1,21 @@
 import { Prisma, User } from "@prisma/client";
-import { prisma } from "../../lib/prisma";
 import { UsersRepository } from "../users-repository";
+import { prisma } from "../../lib/prisma";
 
 export class PrismaResaleRepository implements UsersRepository {
   async create(data: Prisma.UserCreateInput): Promise<User> {
     const user = await prisma.user.create({
       data,
+    });
+
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
     });
 
     return user;
--- src/respositories/users-repository.ts
+++ src/respositories/users-repository.ts
@@ -1,5 +1,6 @@
-import { Prisma, Resale, User } from "@prisma/client";
+import { Prisma, User } from "@prisma/client";
 
 export interface UsersRepository {
   create(data: Prisma.UserCreateInput): Promise<User>;
+  findByEmail(email: string): Promise<User | null>;
 }
