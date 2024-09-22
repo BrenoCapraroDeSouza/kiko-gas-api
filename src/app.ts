ab84c14165d86ba0debabf5fa8c248881e1ad0e5
RafaelMotaAlvess <leafardzn@gmail.com>
2024-09-21 21:19:02 -0300

    chore: add fastify cookie and jwt dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 import fastify from "fastify";
 import { resalesRoutes } from "./http/controllers/resales/routes";
+import fastifyJwt from "@fastify/jwt";
+import { env } from "./env";
+import fastifyCookie from "@fastify/cookie";
 
 export const app = fastify();
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
+});
+
+app.register(fastifyCookie);
 
 app.register(resalesRoutes);
