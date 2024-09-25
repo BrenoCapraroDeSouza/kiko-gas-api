569416aa727959128c38c7e405313b325e55456c
RafaelMotaAlvess <leafardzn@gmail.com>
2024-09-25 20:07:52 -0300

    feat: add route to refresh token

---
+++ src/http/controllers/users/refresh-controller.ts
@@ -0,0 +1,42 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export async function refresh(request: FastifyRequest, response: FastifyReply) {
+  await request.jwtVerify({
+    onlyCookie: true,
+  });
+
+  const user = request.user as any;
+
+  const { type } = user;
+
+  const token = await response.jwtSign(
+    { type },
+    {
+      sign: {
+        sub: user.id,
+      },
+    }
+  );
+
+  const refreshToken = await response.jwtSign(
+    { type },
+    {
+      sign: {
+        sub: user.id,
+        expiresIn: "7d",
+      },
+    }
+  );
+
+  return response
+    .setCookie("refreshToken", refreshToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: true,
+      secure: true,
+    })
+    .status(200)
+    .send({
+      token,
+    });
+}
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { authenticate } from "./authenticate-controller";
+import { refresh } from "./refresh-controller";
 
 export async function usersRoute(app: FastifyInstance) {
   app.post("/login", authenticate);
+  app.post("/refresh", refresh);
 }
