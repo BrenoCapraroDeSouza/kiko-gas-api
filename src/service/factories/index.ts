b437084ea36f2844fe11d2f68e099fc3d3876862
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-09-28 22:00:48 -0300

    feat: add client fetch export

--- src/service/factories/index.ts
+++ src/service/factories/index.ts
@@ -1,3 +1,4 @@
 export * from "./make-authenticate-user-service";
 export * from "./make-register-client-service";
 export * from "./make-register-resale-service";
+export * from "./make-fetch-client-service";
