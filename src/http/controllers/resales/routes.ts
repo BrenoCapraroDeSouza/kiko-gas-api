d285d39c2f9a48a25a94bb984f7e315c421f8281
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-09-28 22:57:18 -0300

    feat: add fetch route for resales

--- src/http/controllers/resales/routes.ts
+++ src/http/controllers/resales/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./register-controller";
+import { findAll } from "./fetch-controller";
 
 export async function resalesRoutes(app: FastifyInstance) {
   app.post("/resales", register);
+  app.get("/resales", findAll);
 }
