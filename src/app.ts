73f0c8a1b37d5984271adaee9b43cf59879f7dfa
RafaelMotaAlvess <leafardzn@gmail.com>
2024-09-29 15:27:34 -0300

    chore: add fastify cors

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,14 @@ import { env } from "./env";
 import { resalesRoutes } from "./http/controllers/resales/routes";
 import { clientsRoutes } from "./http/controllers/clients/routes";
 import { usersRoute } from "./http/controllers/users/routes";
+import fastifyCors from "@fastify/cors";
 
 export const app = fastify();
+
+app.register(fastifyCors, {
+  origin: true,
+  credentials: true,
+});
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
