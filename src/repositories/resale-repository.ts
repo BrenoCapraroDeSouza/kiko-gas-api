4cac574703320e1352cea8cf6c3ee3fe906d6b3e
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-09-28 22:58:14 -0300

    refactor: update findAll method in resales repository

--- src/repositories/resale-repository.ts
+++ src/repositories/resale-repository.ts
@@ -4,5 +4,5 @@ export interface ResaleRepository {
   create(data: Prisma.ResaleCreateInput): Promise<Resale>;
   findById(id: string): Promise<Resale | null>;
   update(id: string, data: Prisma.ResaleUpdateInput): Promise<Resale | null>;
-  findAll(): Promise<Resale[]>;
+  findAll(page?: number, pageSize?: number): Promise<Resale[]>;
 }
