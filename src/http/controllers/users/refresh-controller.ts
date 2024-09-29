e4c634e0d84042e5f29188e2d10c83dfd5f15d2e
RafaelMotaAlvess <leafardzn@gmail.com>
2024-09-29 18:42:27 -0300

    feat: extract bearer token from header

--- src/http/controllers/users/refresh-controller.ts
+++ src/http/controllers/users/refresh-controller.ts
@@ -2,47 +2,64 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { makeGetUserService } from "../../../service/factories/make-get-user-service";
 
 export async function refresh(request: FastifyRequest, response: FastifyReply) {
-  await request.jwtVerify({
-    onlyCookie: true,
-  });
+  const authorizationHeader = request.headers.authorization;
 
-  const userParams = request.user as any;
+  if (!authorizationHeader) {
+    return response
+      .status(401)
+      .send({ error: "No Authorization header provided" });
+  }
 
-  const { type, userId } = userParams;
+  const [scheme, token] = authorizationHeader.split(" ");
 
-  const userService = makeGetUserService();
+  if (scheme !== "Bearer" || !token) {
+    return response
+      .status(401)
+      .send({ error: "Invalid Authorization header format" });
+  }
 
-  const user = (await userService.execute({ userId })) as any;
+  try {
+    const userParams = request.server.jwt.verify(token);
 
-  const token = await response.jwtSign(
-    { type },
-    {
-      sign: {
-        sub: userId,
-      },
-    }
-  );
+    const { type, userId } = userParams as any;
 
-  const refreshToken = await response.jwtSign(
-    { type, userId },
-    {
-      sign: {
-        sub: userId,
-        expiresIn: "7d",
-      },
-    }
-  );
+    const userService = makeGetUserService();
 
-  return response
-    .setCookie("refreshToken", refreshToken, {
-      path: "/",
-      httpOnly: true,
-      sameSite: true,
-      secure: true,
-    })
-    .status(200)
-    .send({
-      name: user?.resale?.name || user?.client?.name,
-      token,
-    });
+    const user = (await userService.execute({ userId })) as any;
+
+    const newToken = await response.jwtSign(
+      { type },
+      {
+        sign: {
+          sub: userId,
+        },
+      }
+    );
+
+    // Gera um novo refresh token
+    const newRefreshToken = await response.jwtSign(
+      { type, userId },
+      {
+        sign: {
+          sub: userId,
+          expiresIn: "7d",
+        },
+      }
+    );
+
+    return response
+      .setCookie("refreshToken", newRefreshToken, {
+        path: "/",
+        httpOnly: true,
+        sameSite: true,
+        secure: true,
+      })
+      .status(200)
+      .send({
+        name: user?.resale?.name || user?.client?.name,
+        token: newToken, // Retorna o novo token
+      });
+  } catch (error) {
+    return response.status(401).send({ error: "Invalid or expired token" });
+  }
 }
