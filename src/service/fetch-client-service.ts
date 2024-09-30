159fcba8756b191457cf45d3954d2dc7cf083f61
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-09-29 22:48:06 -0300

    refactor: update findAll method signature in ClientRepository and add resaleId parameter in FetchClientRequest

--- src/service/fetch-client-service.ts
+++ src/service/fetch-client-service.ts
@@ -3,6 +3,7 @@ import { ClientRepository } from "../repositories/client-repository";
 import { Client } from "@prisma/client";
 
 interface FetchClientRequest {
+  resaleId
   page?: number;
   pageSize?: number;
 }
@@ -15,6 +16,7 @@ export class FetchClientService {
   async execute(data: FetchClientRequest): Promise<Client[]> {
 
     const client: Client[] = await this.clientRepository.findAll(
+        data.resaleId,
         data.page || 1,
         data.pageSize || 10
     );
