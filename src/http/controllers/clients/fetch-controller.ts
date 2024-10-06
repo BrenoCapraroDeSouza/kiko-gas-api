bab2d2a21d56695030e8d9835c8081df58d08b82
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-10-05 21:12:04 -0300

    refactor: remove unnecessary authentication

--- src/http/controllers/clients/fetch-controller.ts
+++ src/http/controllers/clients/fetch-controller.ts
@@ -1,5 +1,8 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { makeFetchClientService } from "../../../service/factories";
+import { verifyJWT } from "../../middlewares/verify-jwt";
+import { verifyUserType } from "../../middlewares/verify-user-type";
+import { makeGetUserService } from "../../../service/factories/make-get-user-service";
 
 export async function findAll(
     request: FastifyRequest, response: FastifyReply
@@ -7,10 +10,22 @@ export async function findAll(
 
     const { page = 1, pageSize = 10 } = request.query as any;
 
+    const userId = request.user.userId;
+
+    const getUserService =  makeGetUserService();
+
+    const user = await getUserService.execute({ userId });
+
+    if (!user) return response.code(404).send({ message: "User not found" });
+
+    if (!user.resale) return response.code(404).send({ message: "Resale not found for user" });
+    
+    const resaleId = user.resale.id;
+
     const fetchClientService = makeFetchClientService();
 
     try {
-        const clients = await fetchClientService.execute({ page, pageSize });
+        const clients = await fetchClientService.execute({ resaleId, page, pageSize });
         response.status(200).send(clients);
     } catch (error) {
         response.code(500).send({ message: error.message });
