60677c768b7c2019d04341f88f7c65d85a6e5a5a
RafaelMotaAlvess <leafardzn@gmail.com>
2024-10-08 19:46:54 -0300

    refactor: add query param interface

--- src/http/controllers/clients/fetch-controller.ts
+++ src/http/controllers/clients/fetch-controller.ts
@@ -1,33 +1,38 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { makeFetchClientService } from "../../../service/factories";
-import { verifyJWT } from "../../middlewares/verify-jwt";
-import { verifyUserType } from "../../middlewares/verify-user-type";
 import { makeGetUserService } from "../../../service/factories/make-get-user-service";
 
-export async function findAll(
-    request: FastifyRequest, response: FastifyReply
-) {
+export interface QueryParams {
+  page?: number;
+  pageSize?: number;
+}
 
-    const { page = 1, pageSize = 10 } = request.query as any;
+export async function findAll(request: FastifyRequest, response: FastifyReply) {
+  const { page = 1, pageSize = 10 } = request.query as QueryParams;
 
-    const userId = request.user.userId;
+  const userId = request.user.userId;
 
-    const getUserService =  makeGetUserService();
+  const getUserService = makeGetUserService();
 
-    const user = await getUserService.execute({ userId });
+  const user = await getUserService.execute({ userId });
 
-    if (!user) return response.code(404).send({ message: "User not found" });
+  if (!user) return response.code(404).send({ message: "User not found" });
 
-    if (!user.resale) return response.code(404).send({ message: "Resale not found for user" });
+  if (!user.resale)
+    return response.code(404).send({ message: "Resale not found for user" });
 
-    const resaleId = user.resale.id;
+  const resaleId = user.resale.id;
 
-    const fetchClientService = makeFetchClientService();
+  const fetchClientService = makeFetchClientService();
 
-    try {
-        const clients = await fetchClientService.execute({ resaleId, page, pageSize });
-        response.status(200).send(clients);
-    } catch (error) {
-        response.code(500).send({ message: error.message });
-    }
+  try {
+    const clients = await fetchClientService.execute({
+      resaleId,
+      page,
+      pageSize,
+    });
+    response.status(200).send(clients);
+  } catch (error) {
+    response.code(500).send({ message: error.message });
+  }
 }
