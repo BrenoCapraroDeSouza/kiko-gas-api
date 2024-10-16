5146afafe2304890ddfe6ab5b73a483b896bd16f
RafaelMotaAlvess <leafardzn@gmail.com>
2024-10-16 20:16:22 -0300

    refactor: add error message when not found

--- src/http/controllers/clients/register-address-controller.ts
+++ src/http/controllers/clients/register-address-controller.ts
@@ -2,36 +2,42 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 import { makeRegisterClientAddressService } from "../../../service/factories/make-register-client-address-service";
 
-
 export async function registerAddress(
-    request: FastifyRequest,
-    response: FastifyReply
-){
-    const registerBodySchema = z.object({
-        name: z.string(),
-        address: z.string(),
+  request: FastifyRequest,
+  response: FastifyReply
+) {
+  const registerBodySchema = z
+    .object({
+      name: z.string(),
+      address: z.string(),
     })
     .strict();
 
-    const userId = request.user?.userId;
+  const userId = request.user?.userId;
 
-    if (!userId) {
-        throw new Error("Usuário não autenticado");
+  if (!userId) {
+    throw new Error("Usuário não autenticado");
+  }
+
+  const addressData = registerBodySchema.parse(request.body);
+
+  const registerClientAddressService = makeRegisterClientAddressService();
+
+  try {
+    const updatedClient = await registerClientAddressService.execute({
+      clientId: userId,
+      addressData,
+    });
+
+    response.status(201).send(updatedClient);
+  } catch (error) {
+    console.error(error);
+
+    if (error.message === "Cliente não encontrado!") {
+      response.status(404).send({ message: "Cliente não encontrado!" });
+      return;
     }
 
-    const addressData = registerBodySchema.parse(request.body);
-
-    const registerClientAddressService = makeRegisterClientAddressService();
-
-    try {
-        const updatedClient = await registerClientAddressService.execute({
-            clientId: userId,
-            addressData,
-        });
-        
-        response.status(201).send(updatedClient);
-    } catch (error) {
-        console.error(error);
-        response.status(500).send({ message: "Internal Server Error" });
-    }
+    response.status(500).send({ message: "Internal Server Error" });
+  }
 }
