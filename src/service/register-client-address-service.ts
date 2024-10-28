f008d0e70766536673105d2be72aac263729b66a
RafaelMotaAlvess <leafardzn@gmail.com>
2024-10-28 20:11:40 -0300

    refactor: add id field to client address

--- src/service/register-client-address-service.ts
+++ src/service/register-client-address-service.ts
@@ -1,5 +1,6 @@
 import { UserRepository } from "../repositories/user-repository";
 import { ClientRepository } from "../repositories/client-repository";
+import { ObjectId } from "mongodb";
 
 interface AddressData {
   name: string;
@@ -18,19 +19,22 @@ export class RegisterClientAddressService {
   ) {}
 
   async execute({ clientId, addressData }: RegisterClientAddressRequest) {
-
     const { client } = await this.userRepository.findById(clientId);
 
-    if(!client) {
-        throw new Error("Cliente não encontrado!");
+    if (!client) {
+      throw new Error("Cliente não encontrado!");
     }
 
     const { name, address } = addressData;
 
-    const updatedClient = await this.clientRepository.registerAddress(client.id, {
+    const updatedClient = await this.clientRepository.registerAddress(
+      client.id,
+      {
+        id: new ObjectId().toString(),
         name,
         address,
-    });
+      }
+    );
 
     return updatedClient;
   }
